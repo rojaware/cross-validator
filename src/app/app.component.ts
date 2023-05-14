412ab20e370976ce275544c9ada9a484afda8c74
scott lee <leesungki@gmail.com>
2023-05-14 09:28:49 -0400

    no

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Room } from './rooms';
+import { AbstractControl } from '@angular/forms';
+import { RoomOver18Validator } from './room-over-18.validator';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'forms-cross-field-validation';
+  form: FormGroup;
+  isSubmitted: boolean;
+
+  rooms: Room[] = [
+    { text: 'room 1', value: 'room-1' },
+    { text: 'room 2', value: 'room-2' },
+    { text: 'room 3', value: 'room-3' },
+  ];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private roomOver18Validator: RoomOver18Validator
+  ) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        age: ['', [Validators.required, NoNegativeNumbers]],
+        room: [{}, Validators.required],
+      },
+      {
+        validators: [this.roomOver18Validator.onlyAccessRoomsOver18(18)],
+        updateOn: 'blur',
+      }
+    );
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.isSubmitted = true;
+  }
+}
+
+export function NoNegativeNumbers(control: AbstractControl) {
+  return control.value < 0 ? { negativeNumber: true } : null;
+}
